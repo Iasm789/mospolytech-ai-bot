4a8fe999d84ed1217a5b78626b3f595b97e3978a
Abdulboriy Isamidinov <iasm.78.a@gmail.com>
2026-02-14 00:55:45 +0300

    delete deepseek

--- config/settings.py
+++ config/settings.py
@@ -12,20 +12,6 @@ class Settings(BaseSettings):
     BOT_TOKEN: str = Field(..., env='BOT_TOKEN')
     DEBUG: bool = Field(default=False, env='DEBUG')
     
-    # GigaChat API
-    GIGACHAT_CLIENT_ID: str = Field(default='', env='GIGACHAT_CLIENT_ID')
-    GIGACHAT_CLIENT_SECRET: str = Field(default='', env='GIGACHAT_CLIENT_SECRET')
-    GIGACHAT_SCOPE: str = Field(default='GIGACHAT_API_PERS', env='GIGACHAT_SCOPE')
-    GIGACHAT_AUTH_URL: str = Field(
-        default='https://ngw.devices.sberbank.ru:9443/api/v2/oauth',
-        env='GIGACHAT_AUTH_URL'
-    )
-    GIGACHAT_API_URL: str = Field(
-        default='https://gigachat.devices.sberbank.ru/api/v1',
-        env='GIGACHAT_API_URL'
-    )
-    GIGACHAT_MODEL: str = Field(default='GigaChat', env='GIGACHAT_MODEL')
-    
     # Logging
     LOG_LEVEL: str = Field(default='INFO', env='LOG_LEVEL')
     
